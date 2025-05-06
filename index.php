e1684e709da83de5577a7d560ff261dbfa0e129f
Angelica265 <gelicalaab@gmail.com>
2025-05-07 00:58:05 +0800

    Update index.php

--- index.php
+++ index.php
@@ -110,7 +110,7 @@ if($authenticated){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>NIDOY_LD1_FINALS</title>
+    <title>LAAB_REGISTRATION_SYSTEM</title>
     <style>
         body {
             font-family: sans-serif;
